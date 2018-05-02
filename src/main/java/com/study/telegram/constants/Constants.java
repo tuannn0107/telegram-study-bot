038eda767c694c7d6dfe66c0c6a9fef62726ba67
tuannn0107 <tuannn0107@gmail.com>
2018-05-02 19:47:53 +0700

    change token format

--- src/main/java/com/study/telegram/constants/Constants.java
+++ src/main/java/com/study/telegram/constants/Constants.java
@@ -4,10 +4,12 @@ public final class Constants {
     //****************************** TOKEN ******************************//
     //*******************************************************************//
     public static final String BOT_FORECAST_NAME = "Forecast Bot";
-    public static final String BOT_FORECAST_TOKEN = "587939381:AAFw4n_42P-wu8EVt11VG5RYuQ8LC9d1F_M";
+    public static final String BOT_FORECAST_TOKEN_ID = "587939381";
+    public static final String BOT_FORECAST_TOKEN = BOT_FORECAST_TOKEN_ID + ":AAFw4n_42P-wu8EVt11VG5RYuQ8LC9d1F_M";
 
     public static final String BOT_BITTREX_PREDICT_NAME = "Bittrex predict Bot";
-    public static final String BOT_BITTREX_PREDICT_TOKEN = "577867614:AAH_fZcW0huIIK-hRRTRxQGLFzuhAOG0V14";
+    public static final String BOT_BITTREX_PREDICT_TOKEN_ID = "577867614";
+    public static final String BOT_BITTREX_PREDICT_TOKEN = BOT_BITTREX_PREDICT_TOKEN_ID + ":AAH_fZcW0huIIK-hRRTRxQGLFzuhAOG0V14";
 
 
     //****************************** SYMBOL ******************************//
