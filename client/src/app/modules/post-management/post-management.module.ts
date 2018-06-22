8b4618eb3ca84f4b8125338758bdb78228765a66
Joey <tuannn0107@gmail.com>
2018-06-22 17:06:05 +0700

    cm

--- client/src/app/modules/post-management/post-management.module.ts
+++ client/src/app/modules/post-management/post-management.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PostManagementRoutingModule } from './post-management-routing.module';
+import {MatPaginatorModule} from '@angular/material/paginator';
 import { PostManagementComponent } from './components/post-management/post-management.component';
 import { PostManagementService } from './services/post-management.service';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 @NgModule({
   imports: [
     CommonModule,
-    PostManagementRoutingModule
+    PostManagementRoutingModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
   declarations: [PostManagementComponent],
   providers: [PostManagementService]
